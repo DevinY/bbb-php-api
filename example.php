b0466af7893f83aea5bf90ad7ea0c53160f509af
Devin Yang <devin@ccc.tc>
2018-01-02 01:03:00 +0800

    Fix Example Typo

--- example.php
+++ example.php
@@ -1,6 +1,6 @@
 <?php
-use Deviny\BigBlueButtonPhpApi\Bbb;
-include(__DIR__."/src/Bbb.php");
+use DevinY\BigBlueButtonPhpApi\Bbb;
+require __DIR__.'/../vendor/autoload.php';
 define("DEFAULT_SECURITY_SALT", "abcdefg");
 define("DEFAULT_SERVER_BASE_URL", "https://<your-domain>/bigbluebutton/");
 
